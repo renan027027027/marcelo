3a5900031f6463cf415422d0822f1d5045becb15
renan027027027 <digitalstore027@gmail.com>
2023-11-24 12:48:44 -0300

    sadasdasd

---
+++ api/index.php
@@ -0,0 +1,16 @@
+<?php
+
+header('Content-Type: application/json');
+
+$numero = "12" + "12";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php echo $numero; ?></title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+VIEIRA  </body>
+</html>
